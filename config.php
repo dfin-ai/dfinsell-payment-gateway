029ba805cbb5f27df0b599f0c2b7cb0daab889e0
root <root@wp-server-dfincapital.gcw55aitodlezdiiqosga3h1hg.cx.internal.cloudapp.net>
2025-12-10 18:14:37 +0000

    for test

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// config.php
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+return [
+    'id'   => 'dfinsell',
+    'name' => 'DFin Sell Payment Gateway',
+    'version' => '1.1.5',
+	'title' => 'DFin Sell Payment Gateway',
+    'description' => 'This plugin allows you to accept payments in USD through a secure payment gateway integration. Customers can complete their payment process with ease and security',
+    'icon' => '',
+
+    'protocol' => is_ssl() ? 'https://' : 'http://',
+    'host'     => 'sell-staging.dfin.ai',
+
+    'requirements' => [
+        'php' => '8.0',
+        'wc'  => '6.5.4',
+    ],
+
+    'paths' => [
+		'file'   => __FILE__,                       // Full path to this file
+        'dir'    => plugin_dir_path(__FILE__),     // Absolute dir path
+        'url'    => plugin_dir_url(__FILE__),      // Base URL
+        'assets' => plugin_dir_url(__FILE__) . 'assets/', // Assets URL
+    ],
+];
